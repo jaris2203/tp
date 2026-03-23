95d7b5207707c08fbd150fc001150d5479261b05
cassandratay <cassandratayzx@gmail.com>
2026-03-24 00:33:33 +0800

    Implement clustering based on postal code prefixes

--- src/main/java/seedu/address/model/util/ClusterUtil.java
+++ src/main/java/seedu/address/model/util/ClusterUtil.java
@@ -61,4 +61,46 @@ public class ClusterUtil {
     public static int getPostalPrefixFromPerson(Person person) {
         return getPostalPrefixFromAddress(person.getAddress().value);
     }
+
+    /**
+     * Groups a list of Persons into clusters based on their postal code prefixes and distributes them among a specified number of drivers.
+     *
+     * @param list The list of Persons to be grouped.
+     * @param numDrivers The number of drivers to distribute the clusters among.
+     * @return A list of lists, where each inner list contains the Persons assigned to a driver.
+     * @throws IllegalArgumentException if numDrivers is less than or equal to 0.
+     */
+    public static List<List<Person>> groupIntoClusters(List<Person> list, int numDrivers) {
+        if (numDrivers <= 0) {
+            throw new IllegalArgumentException("Number of drivers must be a positive integer.");
+        }
+
+        if (list == null || list.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        List<Person> sortedList = new ArrayList<>(list);
+        sortedList.sort((person1, person2) -> {
+            int prefix1 = getPostalPrefixFromPerson(person1);
+            int prefix2 = getPostalPrefixFromPerson(person2);
+            return Integer.compare(prefix1, prefix2); // sort persons by postal prefix ascending
+        });
+
+        List<List<Person>> driverAssignments = new ArrayList<>();
+        int numPersons = sortedList.size();
+        int minPerDriver = numPersons / numDrivers;
+        int remainder = numPersons % numDrivers;
+
+        int startIndex = 0;
+        for (int i = 0; i < numDrivers; i++) {
+            int groupSize = minPerDriver + (i < remainder ? 1 : 0);
+            List<Person> driverList = new ArrayList<>(
+                    sortedList.subList(startIndex, startIndex + groupSize)
+            );
+            driverAssignments.add(driverList);
+            startIndex += groupSize;
+        }
+
+        return driverAssignments;
+    }
 }
