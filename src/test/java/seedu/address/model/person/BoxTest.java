b5c4d4c00bd0cfb2240c99261d19077e379d3879
Lennon <lemoncode8888@gmail.com>
2026-03-25 00:55:26 +0800

    test: Add tests for compareTo method for box class

--- src/test/java/seedu/address/model/person/BoxTest.java
+++ src/test/java/seedu/address/model/person/BoxTest.java
@@ -1,5 +1,6 @@
 package seedu.address.model.person;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -29,5 +30,21 @@ public class BoxTest {
     public void isValidBoxName() {
         // null box name
         assertThrows(NullPointerException.class, () -> Box.isValidBoxName(null));
+    }
+
+    @Test
+    public void compareTo() {
+        Box box1 = new Box("box-1", new ExpiryDate("2026-12-31"));
+        Box box2 = new Box("box-2", new ExpiryDate("2026-12-31"));
+        Box box1_earlier = new Box("box-1", new ExpiryDate("2026-01-01"));
+
+        // box with earlier expiry date compared to box with later expiry date of the same name -> returns -1
+        assertEquals(-1, box1_earlier.compareTo(box1));
+
+        // box with later expiry date compared to box with earlier expiry date of the same name -> returns 1
+        assertEquals(1, box1.compareTo(box1_earlier));
+
+        // boxNames are in lexographical order -> returns 1
+        assertEquals(-1, box1.compareTo(box2));
     }
 }
