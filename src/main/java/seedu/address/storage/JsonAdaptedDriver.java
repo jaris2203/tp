72d04375436f3190f884827e0be8f6c9fc74ef6f
Jaris Chow <jaris2203@gmail.com>
2026-03-30 12:06:14 +0800

    Fix conflicts with upstream

--- src/main/java/seedu/address/storage/JsonAdaptedDriver.java
+++ src/main/java/seedu/address/storage/JsonAdaptedDriver.java
@@ -5,8 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.delivery.Driver;
-import seedu.address.model.person.Name;
-import seedu.address.model.person.Phone;
+import seedu.address.model.commons.name.Name;
+import seedu.address.model.commons.phone.Phone;
 
 /**
  * Jackson-friendly version of {@link Driver}.
