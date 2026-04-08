fb83cdf351c6be34fd975ac78e161977f3807bf6
Damian <damianeng@damians-MacBook-Air-3.local>
2026-04-09 00:12:31 +0800

    test: Extend AddBoxCommandTest with multiple boxes and equals tests

--- src/test/java/seedu/address/logic/commands/AddBoxCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddBoxCommandTest.java
@@ -1,5 +1,7 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
@@ -73,5 +75,58 @@ public class AddBoxCommandTest {
         AddBoxCommand addBoxCommand = new AddBoxCommand(personToEdit.getName(), boxesToAdd);
         String expectedMessage = String.format(AddBoxCommand.MESSAGE_EXISTING_BOX_NAME, personToEdit.getName());
         assertCommandFailure(addBoxCommand, model, expectedMessage);
+    }
+
+    @Test
+    public void execute_addMultipleBoxes_success() {
+        Person personToEdit = model.getFilteredPersonList().stream()
+                .filter(person -> person.getName().equals(new Name("George Best")))
+                .findFirst()
+                .orElseThrow();
+
+        Set<Box> boxesToAdd = new TreeSet<>(
+                Set.of(new Box("box-1", new ExpiryDate("2026-12-31")),
+                       new Box("box-2", new ExpiryDate("2026-12-30"))));
+        AddBoxCommand addBoxCommand = new AddBoxCommand(personToEdit.getName(), boxesToAdd);
+
+        Set<Box> updatedBoxes = new TreeSet<>(personToEdit.getBoxes());
+        updatedBoxes.addAll(boxesToAdd);
+        Person editedPerson = new Person(personToEdit.getName(), personToEdit.getPhone(), personToEdit.getEmail(),
+                personToEdit.getAddress(), updatedBoxes, personToEdit.getRemark(),
+                personToEdit.getDeliveryStatus(), personToEdit.getTags());
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.setPerson(personToEdit, editedPerson);
+
+        assertCommandSuccess(addBoxCommand, model,
+                String.format(AddBoxCommand.MESSAGE_SUCCESS, boxesToAdd, personToEdit.getName()),
+                expectedModel);
+    }
+
+    @Test
+    public void equals() {
+        Name nameAmy = new Name("Amy Bee");
+        Name nameBob = new Name("Bob Choo");
+        Set<Box> boxes1 = Set.of(new Box("box-1", new ExpiryDate("2026-12-31")));
+        Set<Box> boxes2 = Set.of(new Box("box-2", new ExpiryDate("2026-12-31")));
+
+        AddBoxCommand command = new AddBoxCommand(nameAmy, boxes1);
+
+        // same object -> true
+        assertTrue(command.equals(command));
+
+        // same values -> true
+        assertTrue(command.equals(new AddBoxCommand(nameAmy, boxes1)));
+
+        // different name -> false
+        assertFalse(command.equals(new AddBoxCommand(nameBob, boxes1)));
+
+        // different boxes -> false
+        assertFalse(command.equals(new AddBoxCommand(nameAmy, boxes2)));
+
+        // null -> false
+        assertFalse(command.equals(null));
+
+        // different type -> false
+        assertFalse(command.equals(new ClearCommand()));
     }
 }
