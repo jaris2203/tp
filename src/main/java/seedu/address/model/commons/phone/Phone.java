1de95d8e06455ceec9e21814d8deef762dcc0b4c
Damian <damianeng@damians-MacBook-Air-3.local>
2026-04-11 17:26:21 +0800

    Update error message for number starting with0

--- src/main/java/seedu/address/model/commons/phone/Phone.java
+++ src/main/java/seedu/address/model/commons/phone/Phone.java
@@ -11,7 +11,7 @@ public class Phone {
 
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Phone numbers should only contain numbers, and it should be at least 3 digits long";
+            "Phone numbers should only contain numbers, must be at least 3 digits long, and must not start with 0";
     public static final String VALIDATION_REGEX = "[1-9]\\d{2,}";
     public final String value;
 
